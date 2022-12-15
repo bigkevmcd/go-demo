37ee0c9be105d22edc60722af303fbd4191783c9
Kevin McDermott <bigkevmcd@gmail.com>
2022-12-15 08:35:40 +0000

    Add Acornfile.

--- README.md
+++ README.md
@@ -19,3 +19,12 @@ TEST_REDIS_URL=redis://localhost:6379  go test ./...
 ```
 
 If `TEST_REDIS_URL` is _not set_ this will default to `redis://localhost:6379/9`.
+
+## Acorn
+
+Runninng this with [Acorn](https://acorn.io/) is as simple as:
+
+```shell
+$ acorn install
+$ acorn run -n go-demo .
+```
--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -16,12 +16,6 @@ func init() {
 	cobra.OnInitialize(initConfig)
 }
 
-func logIfError(e error) {
-	if e != nil {
-		log.Fatal(e)
-	}
-}
-
 func makeRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "go-demo",
@@ -45,21 +39,21 @@ func makeRootCmd() *cobra.Command {
 		8080,
 		"port to serve requests on",
 	)
-	logIfError(viper.BindPFlag("port", cmd.Flags().Lookup("port")))
+	cobra.CheckErr(viper.BindPFlag("port", cmd.Flags().Lookup("port")))
 
 	cmd.Flags().String(
 		"redis_url",
 		"redis://localhost:6379",
 		"url to connect to Redis on",
 	)
-	logIfError(viper.BindPFlag("redis_url", cmd.Flags().Lookup("redis_url")))
+	cobra.CheckErr(viper.BindPFlag("redis_url", cmd.Flags().Lookup("redis_url")))
 
 	cmd.Flags().String(
 		"redis_key",
 		"demo:value",
 		"key to fetch from Redis",
 	)
-	logIfError(viper.BindPFlag("redis_key", cmd.Flags().Lookup("redis_key")))
+	cobra.CheckErr(viper.BindPFlag("redis_key", cmd.Flags().Lookup("redis_key")))
 	return cmd
 }
 
